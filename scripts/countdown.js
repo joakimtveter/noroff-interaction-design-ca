7fc9b6cd26ab6b4bb3d915f46b4ed4e8be4f38f4
Joakim Tveter <joakim@tveter.net>
2022-09-21 21:37:02 +0200

    Updated timer to future date

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -3,20 +3,19 @@ function countdownTimer() {
     const hoursElement = document.querySelector('.timer.hrs');
     const minutesElement = document.querySelector('.timer.min');
     const secondsElement = document.querySelector('.timer.sec');
-    const target = new Date('2021-11-22T07:00:00').getTime();
+    const target = new Date('2022-11-22T07:00:00').getTime();
     const now = new Date().getTime();
     const remaining = target - now;
-  
+
     let days = Math.floor(remaining / (1000 * 60 * 60 * 24));
     let hours = Math.floor((remaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((remaining % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((remaining % (1000 * 60)) / 1000);
 
-    daysElement.innerHTML = (days >= 10) ? days : `0${days}`;
-    hoursElement.innerHTML = (hours >= 10) ? hours : `0${hours}`;
-    minutesElement.innerHTML = (minutes >= 10) ? minutes : `0${minutes}`;
-    secondsElement.innerHTML = (seconds >= 10) ? seconds : `0${seconds}`;
+    daysElement.innerHTML = days >= 10 ? days : `0${days}`;
+    hoursElement.innerHTML = hours >= 10 ? hours : `0${hours}`;
+    minutesElement.innerHTML = minutes >= 10 ? minutes : `0${minutes}`;
+    secondsElement.innerHTML = seconds >= 10 ? seconds : `0${seconds}`;
 }
 
 setInterval(countdownTimer, 1000);
-
