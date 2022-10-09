3660c2fb1d5f5ec1a1f06ed1364b5be968941e4f
Joakim Tveter <joakim@tveter.net>
2022-10-09 21:59:23 +0200

    Sunday1

--- scripts/main.js
+++ scripts/main.js
@@ -159,7 +159,7 @@ function renderWishlistItems() {
                         <a href="/game.html?gameid=${product.id}"><h2 class="title">${product.title}</h2></a>
                     </div>
                     <div class="reviews">
-                        <div class="review-container">
+                        <div class="review-container dark">
                             <div
                                 class="rating-stars"
                                 style="--rating: ${product.rating}"
@@ -268,22 +268,24 @@ function renderGames() {
                 alt=""
                 data-gameid="${product.id}"
             />
-            <a href="/game.html?gameid=${product.id}">
-              <div class="flex flex-center">
-              <h3 class="product__title">${product.title}</h3>
-            </a>
-            <button class="wishlist-btn sm" data-active="false" data-gameid="${product.id}">
-                    <svg class="wishlist-toggle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512.05 448.77" >
-                        <path class="wishlist-toggle-full" d="M458.35,65c-57.8-48.6-147.1-41.3-202.4,15-55.3-56.3-144.6-63.7-202.4-15-75.2,63.3-64.2,166.5-10.6,221.2l175.4,178.7a52.52,52.52,0,0,0,75.2.1L469,286.25C522.45,231.55,533.65,128.35,458.35,65Z" transform="translate(0.05 -31.98)" />
-                        <path class="wishlist-toggle-line" d="M458.4,64.3C400.6,15.7,311.3,23,256,79.3,200.7,23,111.4,15.6,53.6,64.3-21.6,127.6-10.6,230.8,43,285.5L218.4,464.2a52.52,52.52,0,0,0,75.2.1L469,285.6C522.5,230.9,533.7,127.7,458.4,64.3ZM434.8,251.8,259.4,430.5c-2.4,2.4-4.4,2.4-6.8,0L77.2,251.8c-36.5-37.2-43.9-107.6,7.3-150.7,38.9-32.7,98.9-27.8,136.5,10.5l35,35.7,35-35.7c37.8-38.5,97.8-43.2,136.5-10.6,51.1,43.1,43.5,113.9,7.3,150.8Z" transform="translate(0.05 -31.98)" />
-                    </svg>
-                    <span class="sr-only">Add to wishlist</span>
+            <div class="product__header">
+                <a href="/game.html?gameid=${product.id}">
+                    <h3 class="product__title">${product.title}</h3>
+                </a>
+                <button class="wishlist-btn sm" data-active="false" data-gameid="${product.id}">
+                        <svg class="wishlist-toggle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512.05 448.77" >
+                            <path class="wishlist-toggle-full" d="M458.35,65c-57.8-48.6-147.1-41.3-202.4,15-55.3-56.3-144.6-63.7-202.4-15-75.2,63.3-64.2,166.5-10.6,221.2l175.4,178.7a52.52,52.52,0,0,0,75.2.1L469,286.25C522.45,231.55,533.65,128.35,458.35,65Z" transform="translate(0.05 -31.98)" />
+                            <path class="wishlist-toggle-line" d="M458.4,64.3C400.6,15.7,311.3,23,256,79.3,200.7,23,111.4,15.6,53.6,64.3-21.6,127.6-10.6,230.8,43,285.5L218.4,464.2a52.52,52.52,0,0,0,75.2.1L469,285.6C522.5,230.9,533.7,127.7,458.4,64.3ZM434.8,251.8,259.4,430.5c-2.4,2.4-4.4,2.4-6.8,0L77.2,251.8c-36.5-37.2-43.9-107.6,7.3-150.7,38.9-32.7,98.9-27.8,136.5,10.5l35,35.7,35-35.7c37.8-38.5,97.8-43.2,136.5-10.6,51.1,43.1,43.5,113.9,7.3,150.8Z" transform="translate(0.05 -31.98)" />
+                        </svg>
+                        <span class="sr-only">Add to wishlist</span>
                 </button>
             </div>
-            <p class="product__price">${product.price} kr</p>
-            <button class="add-to-cart" data-gameid="${product.id}">
-              Add to basket
-            </button>
+            <div class="product__footer">
+                <p class="product__price">${product.price} kr</p>
+                <button class="add-to-cart" data-gameid="${product.id}">
+                Add to basket
+                </button>
+            </div>
         </li>`;
     });
     gamesContainer.innerHTML = gamesHTML;
@@ -307,7 +309,7 @@ function renderSingleGame() {
             <img src="${game.image}" alt="${game.title} cover">
         </div>
         <div class="product-body">
-        <div class='flex align-center'>
+        <div class='flex'>
             <h1 class="mb0">${game.title}</h1>
             <button class="wishlist-btn" data-active="false" data-gameid="${game.id}">
                 <svg class="wishlist-toggle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512.05 448.77" >
